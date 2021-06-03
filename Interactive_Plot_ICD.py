0f0682804f4b880cb7f96119fb38a4098a6b4344
MohsenAskar <71376243+MohsenAskar@users.noreply.github.com>
2021-06-03 19:32:40 +0200

    Update Interactive_Plot_ICD.py

--- Interactive_Plot_ICD.py
+++ Interactive_Plot_ICD.py
@@ -6,7 +6,7 @@ import openpyxl # to read .xlsx files
 data = pd.read_excel(r'C:\Users\mas082\OneDrive - UiT Office 365\Desktop\VS_Code\Real_Data_NPR\Book2.xlsx', engine='openpyxl')
 
 data.head()
-# data["color"] = data["color"].astype(str) # use this if you have a numeric variable and want to categorise it
+# data["color"] = data["color"].astype(str) # use this if you have a numeric variable and want to categorize it
 fig = px.scatter(data, x="hovedtilstand", y="Prob. for hosp.", size="Prob. for hosp.", color="color"
                  , size_max=10, title="Probability of staying in a hospital more than 1 day according to diagnosis", hover_name="ICD category") #use hover_name="ICD names" when you attach ICD names to ICD codes
 fig.update_layout(template='plotly_white') # or plotly_dark
